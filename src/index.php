c7ec56c6dd54a301f51797ae24e52729985627e9
Stas <stas.von@gmail.com>
2017-03-26 18:35:38 +0300

    hi

--- src/index.php
+++ src/index.php
@@ -15,7 +15,7 @@
     <body>
         <div class="container">
             <div class="hero-unit">
-                <h1>Hi There!</h1>
+                <h1>Hi There! ReInvent 6:25</h1>
                 <h2>Congratulations</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
